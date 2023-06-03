c6723e179dcc180d742b0f31961ce39cdfea90fe
Spy-599 <125739114+Spy-599@users.noreply.github.com>
2023-06-03 06:48:42 +0300

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -2,17 +2,9 @@
 clear
 figlet -c "CyberCoder"
 echo "Loading.."
-echo " 100%	"
 sleep 1
 
                     	
                             echo "<---------------------Installing Tools--------------------------->"  
                             
                             git clone https://github.com/Spy-599/cyber-coder-v5
-                         
-                            ;;     
-                       
-                            bash SPY.sh
-                            ;;
-                            
-                             exit
