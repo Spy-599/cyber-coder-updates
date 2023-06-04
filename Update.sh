dc471e4dc89490ad79b8c5bf5643ecae413bc1ad
Spy-599 <125739114+Spy-599@users.noreply.github.com>
2023-06-04 08:40:21 +0300

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -7,4 +7,4 @@ sleep 1
                     	
                             echo "<---------------------Installing Tools--------------------------->"  
                             
-                            git clone https://github.com/Spy-599/cyber-coder-v5
+                            git clone https://github.com/Spy-599/cyber-coder-v5-1.o.f
